ab8643e101e3d7ea13f6451330720b7505b4b6d1
Diana Vlasenkova <dianadomino24work@gmail.com>
2020-12-25 21:20:29 +0300

    add contacts, email

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,43 +2,28 @@ import '@fortawesome/fontawesome-free/js/all.min'
 import './index.css'
 import debounce from 'lodash/debounce'
 
-// const projectsButton = document.querySelector('[data-link="#projects"]');
-// const contactsButton = document.querySelector('[data-link="#contact"]');
-// const contactForm = document.querySelector('.form');
-// const errorMessage = contactForm.querySelector('.form__error-message');
-// const submitButton = contactForm.querySelector('.button_type_submit');
-// const formName = contactForm.querySelector('#name');
-// const formEmail = contactForm.querySelector('#email');
-// const formMessage = contactForm.querySelector('#message');
-const toTopButton = document.querySelector('.button_type_to-top');
+const toTopButton = document.querySelector('.button_type_to-top')
 
 const scrollTo = (selector) => {
-  document.getElementById(selector).scrollIntoView(true);
-};
+    document.getElementById(selector).scrollIntoView(true)
+}
 
 const showToTopButton = () => {
-  if (
-    document.body.scrollTop > 400 ||
-    document.documentElement.scrollTop > 400
-  ) {
-    toTopButton.classList.remove('button_hidden');
-  } else {
-    toTopButton.classList.add('button_hidden');
-  }
-};
-
-// projectsButton.addEventListener('click', () => {
-//   scrollTo('projects');
-// });
-// contactsButton.addEventListener('click', () => {
-//   scrollTo('contact');
-// });
+    if (
+        document.body.scrollTop > 400 ||
+        document.documentElement.scrollTop > 400
+    ) {
+        toTopButton.classList.remove('button_hidden')
+    } else {
+        toTopButton.classList.add('button_hidden')
+    }
+}
 
 window.addEventListener(
-'scroll',
-debounce(() => showToTopButton(), 200)
-);
+    'scroll',
+    debounce(() => showToTopButton(), 200)
+)
 
 toTopButton.addEventListener('click', () => {
-scrollTo('header');
-});
+    scrollTo('header')
+})
